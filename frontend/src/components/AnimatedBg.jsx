5fd2207f158090a65edeb4bb471a581771de0cba
Sanskar Mohapatra <sanskarmohapatra@gmail.com>
2021-11-20 01:23:48 +0530

    Landing page animated bg

---
+++ frontend/src/components/AnimatedBg.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./AnimatedBg.css";
+
+export default function AnimatedBg() {
+  return (
+    <>
+      <div className="background-container">
+        <img
+          src="https://www.pinclipart.com/picdir/big/163-1635292_planets-clipart-animated-globe-3d-model-of-earth.png"
+          alt=""
+        />
+        <div class="stars"></div>
+        <div class="twinkling"></div>
+      </div>
+    </>
+  );
+}
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import AnimatedBg from "./AnimatedBg";
 
 export default function Home() {
   const navigate = useNavigate();
@@ -11,17 +12,20 @@ export default function Home() {
   };
   return (
     <div>
-      <div className="home-bar">
-        <h4 className="help-btn" onClick={helpFunc}>
-          Help
-        </h4>
-      </div>
+      <AnimatedBg />
+      {/* <div className="home-bar">
+        
+
+      </div> */}
       <div className="home-mid">
         <div className="home-title">Chainlink</div>
         <div className="home-start" onClick={startFunc}>
           Start
         </div>
       </div>
+      <h4 className="help-btn" onClick={helpFunc}>
+        Help
+      </h4>
     </div>
   );
 }
