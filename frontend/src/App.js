090ce8e99bedbeda070de655ab9af4d063ddd658
Arihant Bansal <17180950+arihantbansal@users.noreply.github.com>
2021-11-26 23:27:18 +0530

    Minor

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,15 +6,15 @@ import Instructions from "./components/Instructions";
 import Character from "./components/Character";
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route path="/help" element={<Instructions />} />
-        <Route path="/character" element={<Character />} />
-      </Routes>
-    </Router>
-  );
+	return (
+		<Router>
+			<Routes>
+				<Route exact path="/" element={<Home />} />
+				<Route path="/help" element={<Instructions />} />
+				<Route path="/character" element={<Character />} />
+			</Routes>
+		</Router>
+	);
 }
 
 export default App;
