3cb7dc08c1bbbab044c68b2b978351fd48a85a09
krishanu-xc <75176954+krishanu-xc@users.noreply.github.com>
2021-11-27 18:06:21 +0530

    added links, styling

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,24 +3,31 @@ import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 const Navbar = () => {
-   return (
-      <>
-         <nav className={"navbar"}>
-            <div className={"navbar-logo"}>{"CHAINLINK"}</div>
-            <div className={"navbar-items"}>
-               <span>
-                  <Link to={"/characterlist"}>{"List All"}</Link>
-               </span>
-               <span>
-                  <Link to={"/buycharacter"}>{"Buy New"}</Link>
-               </span>
-               <span>
-                  <Link to={"/wallet"}>{"Wallet"}</Link>
-               </span>
-            </div>
-         </nav>
-      </>
-   );
+  return (
+    <>
+      <nav className={"navbar"}>
+        <div className={"navbar-logo"}>
+          <span>
+            <Link to={"/"}>{"CHAINLINNK"}</Link>
+          </span>
+        </div>
+        <div className={"navbar-items"}>
+          <span>
+            <Link to={"/characterlist"}>{"List All"}</Link>
+          </span>
+          <span>
+            <Link to={"/buycharacter"}>{"Buy New"}</Link>
+          </span>
+          <span>
+            <Link to={"/wallet"}>{"Wallet"}</Link>
+          </span>
+          <span>
+            <Link to={"/mating"}>{"Mating"}</Link>
+          </span>
+        </div>
+      </nav>
+    </>
+  );
 };
 
 export default Navbar;
