cb3acebc343b7b07d4817c46c985c9b6b19443a0
Sanskar Mohapatra <sanskarmohapatra@gmail.com>
2021-11-27 21:43:28 +0530

    styled landing page buttons

--- frontend/src/components/MetamaskButton.jsx
+++ frontend/src/components/MetamaskButton.jsx
@@ -1,31 +1,33 @@
 import React, { useState } from "react";
 import Web3 from "web3";
+import metamaskLogo from "../images/logos/metamask.svg";
 
 const MetamaskButton = () => {
-	const [isOpen, setIsOpen] = useState(false);
-	const [isWalletConnected, setIsWalletConnected] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const [isWalletConnected, setIsWalletConnected] = useState(false);
 
-	const connectWallet = async () => {
-		if (window.ethereum) {
-			const web3 = new Web3(window.ethereum);
-			try {
-				// Request account access if needed
-				await window.ethereum.enable();
-				// Acccounts now exposed
-				return web3;
-			} catch (error) {
-				console.error(error);
-			}
-		}
-	};
+  const connectWallet = async () => {
+    if (window.ethereum) {
+      const web3 = new Web3(window.ethereum);
+      try {
+        // Request account access if needed
+        await window.ethereum.enable();
+        // Acccounts now exposed
+        return web3;
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  };
 
-	return (
-		
-			<>
-				<button onClick={connectWallet}>Connect Metamask</button>
-			</>
-		
-	);
+  return (
+    <>
+      <button onClick={connectWallet} className="metaMask-btn">
+        <img src={metamaskLogo} height={30} alt="metamask logo" />
+        <span className="metaMask-btn-txt">Connect with Metamask</span>
+      </button>
+    </>
+  );
 };
 
 export default MetamaskButton;
